8e532ae8d5bcded6888e5d3a241a345abd0c77f5
hudibaba0001 <123251124+hudibaba0001@users.noreply.github.com>
2025-08-20 22:42:48 +0200

    Update page.tsx

--- app/dashboard/orders/page.tsx
+++ app/dashboard/orders/page.tsx
@@ -1,4 +1,4 @@
-import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
+import { createServerClient } from '@supabase/ssr';
 import { cookies } from 'next/headers';
 import { redirect } from 'next/navigation';
 import OrdersList from './OrdersList';
