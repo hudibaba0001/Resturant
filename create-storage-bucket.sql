96cd5517241b1a8c0638353f1f15e7f67947494b
hudibaba0001 <123251124+hudibaba0001@users.noreply.github.com>
2025-08-30 21:40:17 +0200

    Update create-storage-bucket.sql

--- create-storage-bucket.sql
+++ create-storage-bucket.sql
@@ -1,3 +1,16 @@
 -- Create a public bucket for menu images
 -- Run this in your Supabase SQL editor
-select storage.create_bucket('menu-images', public := true);
+
+-- Method 1: Try the standard storage.create_bucket function
+-- If this fails, use Method 2 below
+INSERT INTO storage.buckets (id, name, public)
+VALUES ('menu-images', 'menu-images', true)
+ON CONFLICT (id) DO NOTHING;
+
+-- Method 2: Alternative approach if the above doesn't work
+-- You can also create the bucket through the Supabase Dashboard:
+-- 1. Go to Storage in your Supabase dashboard
+-- 2. Click "Create a new bucket"
+-- 3. Name it "menu-images"
+-- 4. Make it public
+-- 5. Click "Create bucket"
